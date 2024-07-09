0e48af2a4d6cde9b47801be81c0aa02403a1db10
Ren The Devil Hunter <heavenlyren@gmail.com>
2024-07-09 20:41:20 +0700

    Removed unnecessary comment

--- wrapper.ps1
+++ wrapper.ps1
@@ -1,2 +1,1 @@
-# We will launch the script from github
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/benjamin-rousseau-shift/winget/main/install.ps1'));
