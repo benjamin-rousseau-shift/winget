f86f19e328d01e9055b7b41e4959bda29e8b4c11
Ren The Devil Hunter <heavenlyren@gmail.com>
2024-07-09 21:13:42 +0700

    Added uninstall bloatware

--- install.ps1
+++ install.ps1
@@ -18,8 +18,54 @@ $Apps = @(
   'Git.Git',
   'Discord.Discord'
 )
+$WindowsBloatwares = @(
+  'Clipchamp.Clipchamp',
+  'Microsoft.3DBuilder',
+  'Microsoft.549981C3F5F10', #Cortana app
+  'Microsoft.BingFinance',
+  'Microsoft.BingFoodAndDrink',            
+  'Microsoft.BingHealthAndFitness',        
+  'Microsoft.BingNews',
+  'Microsoft.BingSports',
+  'Microsoft.BingTranslator',
+  'Microsoft.BingTravel',
+  'Microsoft.BingWeather',
+  'Microsoft.Getstarted', # Cannot be uninstalled in Windows 11
+  'Microsoft.Messaging',
+  'Microsoft.Microsoft3DViewer',
+  'Microsoft.MicrosoftJournal',
+  'Microsoft.MicrosoftOfficeHub',
+  'Microsoft.MicrosoftPowerBIForWindows',
+  'Microsoft.MicrosoftSolitaireCollection',
+  'Microsoft.MicrosoftStickyNotes',
+  'Microsoft.MixedReality.Portal',
+  'Microsoft.NetworkSpeedTest',
+  'Microsoft.News',
+  'Microsoft.Office.OneNote',
+  'Microsoft.Office.Sway',
+  'Microsoft.OneConnect',
+  'Microsoft.Print3D',
+  'Microsoft.SkypeApp',
+  'Microsoft.Todos',
+  'Microsoft.WindowsAlarms',
+  'Microsoft.WindowsFeedbackHub',
+  'Microsoft.WindowsMaps',
+  'Microsoft.WindowsSoundRecorder',
+  'Microsoft.XboxApp', # Old Xbox Console Companion App, no longer supported
+  'Microsoft.ZuneVideo',
+  'MicrosoftCorporationII.MicrosoftFamily', # Family Safety App
+  'MicrosoftCorporationII.QuickAssist',
+  'MicrosoftTeams', # Old MS Teams personal (MS Store)
+  'MSTeams'   # New MS Teams app
+)
 $CurrentDateTime = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
 $logFile = "C:\temp\winget-$($CurrentDateTime).log"
+
+# We Uninstall Windows Bloatwares
+foreach ($App in $WindowsBloatwares) {
+  Write-Output "Uninstalling $App"
+  Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
+}
 
 # Install wsl
 wsl --install
--- wrapper.ps1
+++ wrapper.ps1
@@ -1,1 +1,1 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/benjamin-rousseau-shift/winget/main/install.ps1'));
+Powershell -ExecutionPolicy Bypass -Scope Process -Force -Verb RunAs -Command "& Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/benjamin-rousseau-shift/winget/main/install.ps1'));"
