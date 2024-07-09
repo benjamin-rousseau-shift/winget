4d5d020c1d75900ae9f3c016c9adfe38c75d6ec9
Ren The Devil Hunter <heavenlyren@gmail.com>
2024-07-09 20:34:05 +0700

    first commit

---
+++ install.ps1
@@ -0,0 +1,32 @@
+# Variables
+$Apps = @(
+  'jazzdelightsme.WingetPathUpdater',
+  '7zip.7zip',
+  'Brave.Brave',
+  'GOG.Galaxy',
+  'Plex.Plex',
+  'ShareX.ShareX',
+  'Valve.Steam',
+  'VideoLAN.VLC',
+  'Microsoft.VisualStudioCode',
+  'MOTU.MSeries',
+  'Spotify.Spotify',
+  'EpicGames.EpicGamesLauncher',
+  'AntibodySoftware.WizTree',
+  'Notepad++.Notepad++',
+  'FxSoundLLC.FxSound',
+  'Git.Git'
+)
+$CurrentDateTime = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+$logFile = "C:\temp\winget-$($CurrentDateTime).log"
+
+# Create temp directory
+if (-not (Test-Path "C:\temp")) {
+  New-Item -ItemType Directory -Path "C:\temp"
+}
+
+# For each app in the list, install it using winget
+foreach ($App in $Apps) {
+  Write-Output "Installing $App"
+  winget install -e --id $App --silent --accept-package-agreements --accept-source-agreements --log $logFile
+}
