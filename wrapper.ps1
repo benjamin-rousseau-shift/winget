9e2c1b9a17676f54d9f7665c76c5cb31219039d9
Ren The Devil Hunter <heavenlyren@gmail.com>
2024-07-09 20:40:52 +0700

    Repalced iex by Invoke-Expression to avoid confusion

--- wrapper.ps1
+++ wrapper.ps1
@@ -1,2 +1,2 @@
 # We will launch the script from github
-Set-ExecutionPolicy Bypass -Scope Process -Force;iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/benjamin-rousseau-shift/winget/main/install.ps1'));
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/benjamin-rousseau-shift/winget/main/install.ps1'));
